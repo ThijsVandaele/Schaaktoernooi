84225e7a809540550297d58cf6b4286b9f532f1f
chrlemay <chrlemay@gmail.com>
2019-03-26 11:29:43 +0100

    create table List_of_sponsors

---
+++ Schaaktoernooi/Schaaktoernooi/List_of_Sponsors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Schaaktoernooi
+{
+    class List_of_Sponsors
+    {
+        [Key]
+        public int sponsorID { get; set; }
+
+        public string SponsorName { get; set; }
+        public string SponsorPhone { get; set; }
+        public string OtherSponsorDetails { get; set; }
+
+
+    }
+}
